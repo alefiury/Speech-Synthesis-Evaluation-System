4e0cc252eb1b991ac662d36316284257391d7283
alefiury <alefiury14@gmail.com>
2021-05-16 19:45:36 -0300

    Changes

--- config.py
+++ config.py
@@ -2,8 +2,7 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-  SECRET_KEY = os.environ.get('SECRET_KEY') or \
-        '\x01?z!\xc1\xdf\x18\xc7\x8d\x18\xfdU\xf6\xfb\xf5\xbdX:\xbe\xd7\x13\xec\xa8\x7f'
+  SECRET_KEY = os.environ.get('SECRET_KEY')
   FLASK_ADMIN = os.environ.get("FLASKADMIN")
   SQLALCHEMY_TRACK_MODIFICATIONS = False
 
