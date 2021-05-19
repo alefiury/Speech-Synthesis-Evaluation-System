0f3f94d1891de7106ed783baff3d632b6fce4212
alefiury <alefiury14@gmail.com>
2021-05-19 19:19:10 -0300

    Update

--- app/__init__.py
+++ app/__init__.py
@@ -21,6 +21,9 @@ def create_app(config_name):
     db.init_app(app)
     login_manager.init_app(app)
 
+    with app.app_context():
+        db.create_all()
+
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
--- start_app.sh
+++ start_app.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 export FLASK_APP=main.py
 export FLASK_DEBUG=1
 flask run
