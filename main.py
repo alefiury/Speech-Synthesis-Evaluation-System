3adb84b8a3b6124d7a2ef097e4ea4a0c22c127b5
alefiury <alefiury14@gmail.com>
2021-05-19 19:08:43 -0300

    Env config changes

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ from app import create_app, db
 
 from app.main import views
 
-app = create_app(os.getenv('FLASK_CONFIG') or 'production')
+app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 migrate = Migrate(app, db)
