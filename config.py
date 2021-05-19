6491d03dccfc3f26b53707e0ad05cb122e631619
alefiury <alefiury14@gmail.com>
2021-05-19 15:32:43 -0300

    first commit

--- config.py
+++ config.py
@@ -2,7 +2,8 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-  SECRET_KEY = os.environ.get('SECRET_KEY')
+  SECRET_KEY = os.environ.get('SECRET_KEY') or \
+      '\x0c\xc2\xb7k\xf7\xda\x87O\x86\xc7Q\xff\x96?vfR\xc5\x15\x04\xf8\x04rc'
   FLASK_ADMIN = os.environ.get("FLASKADMIN")
   SQLALCHEMY_TRACK_MODIFICATIONS = False
 
