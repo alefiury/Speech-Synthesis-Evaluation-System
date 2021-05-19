7ff271a2ff99c4332d23f8f528acdba2b5111784
alefiury <alefiury14@gmail.com>
2021-05-19 20:59:05 -0300

    Update

--- config.py
+++ config.py
@@ -17,7 +17,7 @@ class DevelopmentConfig(Config):
 
 
 class ProductionConfig(Config):
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("://", "ql://", 1) or \
         'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 
 
