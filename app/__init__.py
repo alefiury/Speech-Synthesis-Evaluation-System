ca68b62604067454db201a170f85af989e868dfd
alefiury <alefiury14@gmail.com>
2021-05-20 03:03:11 -0300

    Database update

--- app/__init__.py
+++ app/__init__.py
@@ -21,13 +21,13 @@ def create_app(config_name):
     db.init_app(app)
     login_manager.init_app(app)
 
-    with app.app_context():
-        db.create_all()
-
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint, url_prefix='/auth')
+
+    with app.app_context():
+        db.create_all()
 
     return app
