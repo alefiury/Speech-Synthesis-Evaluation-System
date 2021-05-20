3ee1b2b519a68121f705e4b7a4da518638e04f4c
alefiury <alefiury14@gmail.com>
2021-05-19 21:05:23 -0300

    Update

--- app/__init__.py
+++ app/__init__.py
@@ -21,8 +21,8 @@ def create_app(config_name):
     db.init_app(app)
     login_manager.init_app(app)
 
-    with app.app_context():
-        db.create_all()
+    # with app.app_context():
+    #     db.create_all()
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
